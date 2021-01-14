a9140c54798d59a782bbabe0570b981c0bbcc213
aokii <3426823@qq.com>
2021-01-14 16:58:13 +0800

    调整日志输出

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import api from "./api/index"
 const { account, password, step } = process.env
 if (account && password) {
     const hours = new Date().getHours();
+    console.log(`当前服务器是${hours}点。`)
     let mStep = Number(step);
     if (mStep) {
         mStep = hours <= 12 ? 10000 : hours <= 20 ? 20000 : 23456;
     }
-    console.log("更新步数：" + mStep);
+    console.log("更新步数为：" + mStep);
     api.updateStep(account, password, mStep, function (resp) {
         console.log(resp);
     });
